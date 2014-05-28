b5e40d7b616cde3fa75cf9dafcdaf4875ec2d825
Ben Jackman <ben@jackman.biz>
2014-05-28 00:10:01 -0500

    Adding shim for JS cross platform
    Signed-off-by: Ben Jackman <ben@jackman.biz>

---
+++ otest-jvm/src/main/scala/cgta/otest/FunSuitePlatformImpl.scala
@@ -0,0 +1,13 @@
+package cgta.otest
+
+
+//////////////////////////////////////////////////////////////
+// Copyright (c) 2014 Ben Jackman, Jeff Gomberg
+// All Rights Reserved
+// please contact ben@jackman.biz or jeff@cgtanalytics.com
+// for licensing inquiries
+// Created by bjackman @ 5/27/14 11:46 PM
+//////////////////////////////////////////////////////////////
+
+
+class FunSuitePlatformImpl
---
+++ otest-sjs/src/main/scala/cgta/otest/FunSuitePlatformImpl.scala
@@ -0,0 +1,17 @@
+package cgta.otest
+
+import scala.scalajs.js.annotation.JSExportDescendentObjects
+
+
+//////////////////////////////////////////////////////////////
+// Copyright (c) 2014 Ben Jackman, Jeff Gomberg
+// All Rights Reserved
+// please contact ben@jackman.biz or jeff@cgtanalytics.com
+// for licensing inquiries
+// Created by bjackman @ 5/27/14 11:46 PM
+//////////////////////////////////////////////////////////////
+
+
+//Todo change to be a trait if possible
+@JSExportDescendentObjects
+class FunSuitePlatformImpl
